989a9a02c90141aed2d3524946ffe9b448be86ff
Evandro Brandão <brownenvan65@gmail.com>
2021-06-28 22:10:04 +0100

    add: show imported transactions after upload file

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -16,6 +16,30 @@ import { HttpClient } from '@angular/common/http';
     <div class="btn-block">
       <button type="button" (click)="importFile()">Importar</button>
     </div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Loja</th>
+      <th scope="col">Dono da Loja</th>
+      <th scope="col">Tipo de Transação</th>
+      <th scope="col">Data de ocorrência</th>
+      <th scope="col">CPF Beneficiário</th>
+      <th scope="col">Número do cartão</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr *ngFor="let trx of transactions; index as i">
+      <th scope="row">{{ i + 1 }}</th>
+      <td>{{ trx.store_name}}</td>
+      <td>{{ trx.owner_name}}</td>
+      <td>{{ trx.transaction_type}}</td>
+      <td>{{ formatDate(trx.occurrency_date)}}</td>
+      <td>{{ trx.cpf_benefited}}</td>
+      <td>{{ trx.card_number}}</td>
+    </tr>
+  </tbody>
+</table>
   </form>
 </div>
 `
@@ -23,6 +47,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'Transações';
   file: any = '';
+
+  transactions: any[] = [];
   constructor(private http: HttpClient) {
 
   }
@@ -34,16 +60,19 @@ export class AppComponent {
   }
   importFile() {
     const formData = new FormData();
-    console.log(this.file)
     formData.append('file', this.file);
     this.http.post<any>(environment.baseUrl + 'transactions/upload-file', formData)
       .subscribe(
         (res) => {
           if (res.code == 200) {
             alert(res.message);
+            this.transactions = res.data;
           }
         }
       );
+  }
 
+  formatDate(date: any) {
+    return new Date(date).toLocaleString('en-US');
   }
 }
